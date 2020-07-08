1d52bf65a2574af78e565a3adf97892e1fea9715
nemesa <nemesa@starschema.net>
2020-07-08 16:37:35 +0200

    Fix legend title

--- src/layers/layer.ts
+++ src/layers/layer.ts
@@ -271,7 +271,7 @@ export abstract class Layer {
     ): void
     {
         const id = this.getId();
-        const title = roleMap.color(this)
+        const title = roleMap.colorByColorField(settings[this.id].colorField)
         const colorStops = this.getColorStops();
         const format = roleMap.getColumn('color', this.getId()).format;
         legend.addLegend(id, title, colorStops, format);
